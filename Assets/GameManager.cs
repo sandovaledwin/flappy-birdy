f75021bbf3e7908f63c00724ce72f9c918501539
Edwin Sandoval <sandovaledwin@ITs-MacBook-Pro.local>
2019-11-13 15:18:55 -0600

    Initial Base - Flappy Bird project

---
+++ Assets/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject gameOverCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void gameOver()
+    {
+        gameOverCanvas.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void replay()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void start()
+    {
+        gameOverCanvas.SetActive(false);
+    }
+}
---
+++ Assets/SoundManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static AudioClip birdFlying;
+    private static AudioSource audioSrc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        birdFlying = Resources.Load<AudioClip>("Sounds/BirdJump.wav");
+
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void PlaySound()
+    {
+        audioSrc.PlayOneShot(birdFlying);
+    }
+}
